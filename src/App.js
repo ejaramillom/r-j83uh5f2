b3b6ef12131000a8ed485aa0ad65802e95b9051c
ejaramillom <ejaramilo@hotmail.com>
2019-11-05 23:37:12 -0500

    count characters

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor (props){
+    super (props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      count:  0
+    }
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3"
+          ref={ref => (this.textarea = ref)}
+          onInput={this.handleChange}
+        >
+        </textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
+  }
+
+  handleChange(){
+    this.setState({
+      count: this.textarea.value.length,
+    });
   }
 }
 
