097888260dc56c0994efff123c2d8e43c007719c
ejaramillom <ejaramilo@hotmail.com>
2019-11-06 21:23:36 -0500

    changed counter objective

--- src/App.js
+++ src/App.js
@@ -3,29 +3,26 @@ import './App.css';
 
 class App extends Component {
   constructor (props){
-    super (props);
+    super(props);
     this.handleChange = this.handleChange.bind(this);
     this.state = {
-      count:  0
+      value:  0
     }
   }
 
   render() {
     return (
       <div className="container">
-        <textarea rows="3"
-          ref={ref => (this.textarea = ref)}
-          onInput={this.handleChange}
-        >
+        <textarea rows="3" onChange={this.handleChange} >
         </textarea>
-        <div className="counter">{this.state.count}</div>
+        <div className="counter">{this.state.value}</div>
       </div>
     );
   }
 
-  handleChange(){
+  handleChange(event){
     this.setState({
-      count: this.textarea.value.length,
+      value: event.target.value.length,
     });
   }
 }
